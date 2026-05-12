1915cf1d53387c3339ca39139d198b28160134f9
Lym <10112818+lym_no_girlfriend@user.noreply.gitee.com>
2026-05-12 16:20:50 +0800

    deploy

--- TCO-DRL_on blockchain/contracts/Test.sol
+++ TCO-DRL_on blockchain/contracts/Test.sol
@@ -1,16 +1,101 @@
 pragma solidity ^0.5.12;
 
 contract Selection {
-    uint action;
-    
-    function ReAction(uint ac) public returns(uint){
-        action=ac;
-        uint ac_d=action;
-        return ac_d;
+    struct SelectionResult {
+        uint requestId;
+        uint methodId;
+        uint8 mode;
+        uint primaryOracle;
+        uint backupOracle;
+        bytes32 policyHash;
+        address submitter;
+        uint timestamp;
     }
 
-    function SeAction() public view returns(uint){
-       uint RecommandAction = action;
-       return RecommandAction;
+    mapping(uint => SelectionResult) public results;
+
+    event SelectionSubmitted(
+        uint indexed requestId,
+        uint indexed methodId,
+        uint8 mode,
+        uint primaryOracle,
+        uint backupOracle,
+        bytes32 policyHash,
+        address submitter,
+        uint timestamp
+    );
+
+    function submitSelection(
+        uint requestId,
+        uint methodId,
+        uint8 mode,
+        uint primaryOracle,
+        uint backupOracle,
+        bytes32 policyHash
+    )
+        public
+        returns (bool)
+    {
+        results[requestId] = SelectionResult(
+            requestId,
+            methodId,
+            mode,
+            primaryOracle,
+            backupOracle,
+            policyHash,
+            msg.sender,
+            now
+        );
+
+        emit SelectionSubmitted(
+            requestId,
+            methodId,
+            mode,
+            primaryOracle,
+            backupOracle,
+            policyHash,
+            msg.sender,
+            now
+        );
+
+        return true;
+    }
+
+    function getSelection(uint requestId)
+        public
+        view
+        returns (
+            uint methodId,
+            uint8 mode,
+            uint primaryOracle,
+            uint backupOracle,
+            bytes32 policyHash,
+            address submitter,
+            uint timestamp
+        )
+    {
+        SelectionResult storage r = results[requestId];
+
+        return (
+            r.methodId,
+            r.mode,
+            r.primaryOracle,
+            r.backupOracle,
+            r.policyHash,
+            r.submitter,
+            r.timestamp
+        );
+    }
+
+    // Backward-compatible interface for the original code.
+    uint action;
+
+    function ReAction(uint ac) public returns(uint) {
+        action = ac;
+        return action;
+    }
+
+    function SeAction() public view returns(uint) {
+        return action;
     }
 }
